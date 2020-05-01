da8b02f0c3af7b725e3a51d8afa0b9446e285c1e
Owen Mundy <omundy@gmail.com>
2020-05-01 16:06:53 -0400

    Changes home page to bootstrap cards

--- index.php
+++ index.php
@@ -8,37 +8,30 @@
     <div class="container">
         <div class="row">
 
-            <div class="col-sm">
-                <a href="papers/gerig/" title="">
-                    <div class="project-thumb">
-                        <div class="thumb"><img src="papers/gerig/img/gerig-6x4.png" alt=""></div>
-                        <h5 class="title">Drinking the Kool-Aid</h5>
-                        <div class="author">Ginny Gerig</div>
-                    </div>
-                </a>
+            <div class="card bg-dark text-white" style="width: 18rem;">
+                <img class="card-img-top" src="papers/de-angelis/img/thumb-6x4.png" alt="thumbnail">
+                <div class="card-body">
+                    <a href="papers/de-angelis/" title="" class="stretched-link">
+                        <h4 class="card-title">If many are doing something, it must be worthwhile</h4>
+                        <h6>Vittoria De Angelis</h6>
+                        <p class="card-text text-muted">Social proof as a persuasion technique.</p>
+                    </a>
+                </div>
             </div>
 
-            <div class="col-sm">
-                <a href="papers/sample" title="">
-                    <div class="project-thumb">
-                        <div class="thumb"></div>
-                        <div class="title">Title of the project</div>
-                        <div class="author">First Last</div>
-                    </div>
-                </a>
+            <div class="card bg-dark text-white" style="width: 18rem;">
+                <img class="card-img-top" src="papers/gerig/img/thumb-6x4.png" alt="thumbnail">
+                <div class="card-body">
+                    <a href="papers/gerig/" title="" class="stretched-link">
+                        <h4 class="card-title">Drinking the Kool-Aid</h4>
+                        <h6>Ginny Gerig</h6>
+                        <p class="card-text text-muted">Cult recruiting, brainwashing, and how followers recover from this extreme persuasion.</p>
+                    </a>
+                </div>
             </div>
 
-            <div class="col-sm">
-                <a href="papers/sample" title="">
-                    <div class="project-thumb">
-                        <div class="thumb"></div>
-                        <div class="title">Title of the project</div>
-                        <div class="author">First Last</div>
-                    </div>
-                </a>
-            </div>
+
         </div>
-
     </div>
 
 
