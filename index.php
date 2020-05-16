d22d73620d78a805a4ecc7c6979cad4b3a4171ea
Owen Mundy <omundy@gmail.com>
2020-05-16 15:57:20 -0400

    Adds Weld

--- index.php
+++ index.php
@@ -102,7 +102,7 @@
 
             <div class="col-sm-12 col-md-6 col-lg-4 col-xl-3 my-3">
                 <div class="card home-card bg-dark text-white h-100">
-                    <img class="card-img-top" src="<?php print($file_root); ?>assets/libs/bootstrap-carousel/placeholder.svg" alt="thumbnail">
+                    <img class="card-img-top" src="papers/weld/img/brain-600x400.png" alt="thumbnail">
                     <div class="card-body">
                         <a href="papers/weld/" title="" class="stretched-link">
                             <h4 class="card-title">Sufferring from Addiction?</h4>
