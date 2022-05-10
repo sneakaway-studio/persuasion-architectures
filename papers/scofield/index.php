18fc402c61579377fc4b4cf32b57225e0cc8993b
Owen Mundy <omundy@gmail.com>
2022-05-10 19:42:51 -0400

    Push everything before change to main

--- papers/scofield/index.php
+++ papers/scofield/index.php
@@ -60,7 +60,7 @@ $paper = returnJsonData("scofield");
 	<div class="container px-0">
 		<div class="row">
 			<div class="col-12">
-				<iframe id="my_iframe" width="100%" height="860px" frameBorder="0" src="game/JustNextDoor.html" scrolling="no"> </iframe>
+				<iframe id="my_iframe" width="100%" height="860px" frameBorder="0" src="game/Build/index.html" scrolling="no"> </iframe>
 
 				<figcaption class="figure-caption">xxxxx</figcaption>
 
