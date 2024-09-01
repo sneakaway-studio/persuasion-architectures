fee375004340532876e7e635472fb29455d4bd79
Owen Mundy <omundy@gmail.com>
2024-09-01 13:14:52 -0400

    Finish Sigl

--- papers/sigl/index.php
+++ papers/sigl/index.php
@@ -4,35 +4,85 @@ $author = "sigl";
 include_once('../../inc/paper-heading.php');
 // print_r($paper);
 
-// NOTES
-// ...
-
 ?>
 
 
 
-
-
-
 <div class="container">
-
     <div class="row my-2">
         <div class="col-12 col-lg-8 offset-lg-2">
+
+			<figure class="figure w-100">
+				<!-- See Figma > Various Projects -->
+				<img src="img/sigl-feed-wireframe.png" alt="thumb" class="figure-img w-100 img-fluid">
+				<!-- <figcaption class="figure-caption">
+				</figcaption> -->
+			</figure>
 
 
 
 <h5 class="subheading">Introduction</h5>
 
+<p>
+Whether you are simply paying a friend back for a meal or starting a new workout regime, the odds are high that your smart device is broadcasting or storing your information. In 2013, PayPal acquired Venmo, an online payment app, and reimagined financial transactions from a private matter into a debatably fun and react-able social activity (Khanna, 2015). Today, Venmo has upwards of 50 million users whose default sharing setting is public, including the sender's name, recipient, and payment description. A quick scroll through the social feed can reveal a person's network, purchasing habits, and social currency. As a Venmo user myself, I can attest to the alluring nature of the social feed because the interface shrouds the seriousness of exchanging money with cool emojis while also presenting other people's spending as a template for my own. 
+</p>
+
+<p>
+While Venmo pioneered the social feed for financial transactions, many other technological sectors have adopted this model. Applications in the health, professional networking, online shopping, personal security, and music streaming sectors have all implemented features that encourage, and in some cases force, users to engage publicly. I think these applications are incredibly clever because they take the most solitary activities of our day, like tracking our fitness, and make them exciting by connecting us with others. However, the social opportunity of apps like Venmo covers up the more nefarious side of this model, which weds us to our phones to keep up with others and perhaps makes us share more than we realize. 
+</p>
+
+<p>
+Technology giants continuously persuade us to give up large amounts of personal information. This paper will explore how these companies leverage peer pressure and the fear of missing out (FOMO) in utilitarian apps to make us share at an alarming rate. As users, we drive business by addictively signaling our consumption and allowing these companies to target the complete demographic profiles created by our chatter. 
+</p>
 
 
 
+<h5 class="subheading">Background</h5>
+
+<p>
+Social media can be described as the “set of technologies and channels targeted at forming and enabling a potentially massive community of participants to productively collaborate” (Albarran, 2013). The earliest and most recognizable form of social media emerged in 2003 with Myspace. Accruing over 14 million users monthly in 2005, Myspace captured public interest with its flexibility for users to create highly personalized homepages. Myspace also cleverly implemented a ranked public display of each user's top eight friends on the site. As a result of these two features, teenage users were led to devote many hours to Myspace, chatting with friends and improving their site. This allure made Myspace a highly attractive medium to target for advertisements. Myspace was successful because of how it harnessed user-created content. 
+</p>
+
+<p>
+As we move into 2022, we can see how the tactic of encouraging user interaction has been adapted and refined with hybrid utilitarian and social applications. No longer with the simple goal of facilitating communication, utilitarian/social apps encourage users to buy, consume, and engage on a public stage. Technology users give up enormous amounts of data every day under pressure to share, either through peer pressure or the drive to remain relevant in our digital world. 
+</p>
 
 
-<!-- https://drive.google.com/drive/folders/1MsgNa5UyDAb4fUkSXgDIe2mA0Cw2Sm3E -->
+<h5 class="subheading">Case Studies</h5>
 
 
+<h6>Spotify</h6>
+
+<p>
+Music streaming has transformed into a wholly different experience with the emergence of Spotify. Spotify offers its users three key functionalities: the ability to curate playlists from a huge library of audio, a strong recommendation algorithm, and facilitated connection with friends. Instead of forcing people to engage outside of the app, Spotify connects users with their Facebook friends as a preset option. You can easily see what music your friends are listening to,  as well as create with your friends using Spotify’s new collaborative playlist function, blend. 
+</p>
+
+<p>
+Spotify has a special leg up from other utility/social app hybrids because it harnesses people’s enthusiasm for music (Vondredau, 2019). Music has great cultural and personal significance to many people, thus making Spotify all the more enticing and agreeable as a streaming platform. 
+</p>
+
+<p>
+Although Spotify has created a handful of noteworthy social features, I am fascinated by how little its developers have done to harness the full networking power of the app. To this point, Spotify has yet to implement a full-size activity feed with posting and commenting ability. Given the universal enthusiasm for music, I would imagine that users would enjoy an in-app space to share playlists, recommendations, and takes on new music. Many UX/UI designers have written blog posts echoing the same confusion about the social media gap in Spotify’s model (Kosmowski, 2020). 
+</p>
+
+<p>
+I wonder if this choice to hold back on the full social media transformation of Spotify is based on legal limitations of artists releasing music on the platform or by some other rationale altogether. Is there value in keeping space between the listener and the musician, reinforcing the idolization of the artist? Or could enhanced communication amongst music enthusiasts detriment the individual listening experience? Perhaps in this specific case, fully submitting to the social feed would push away users rather than lure them in. 
+</p>
 
 
+<h6>Snapchat</h6>
+
+<p>
+Originally developed as a vanishing photo-messaging app, Snapchat has accrued many features over the course of the decade to widen the in-app experience. The creators of the app most notably implemented Snapchat ‘streaks’, which records consecutive days spent mutually snapping with a friend. Snapchat does not share streaks publicly, but rather only shows the count to the people within the pair. 
+</p>
+
+<p>
+The power of gamification and intense social pressure within pairs have made the streaks feature successful. Social media studies scholar, Dayana Hristova, introduced the idea of metacommunication, or the “communication about communication” in relation to Snapchat streaks (Hristova, 2022). By simply being offered as a feature, streaks make pairs on the app more aware and pressured about the timeliness of their interactions. Instead of communicating freely with each other, streaks obligate pairs to interact with each other on a daily regime. It is evident that long streaks do not unlock any reward or reflect the quality of friendship among individuals outside of the app, necessarily. However, the social confirmation of friendship through this tally entraps users because of the shared gamification of the experience within the app (Hristova, 2021). Along with their partner, the user will feel socially compelled to succeed at the non-utilitarian streaks feature.  
+</p>
+
+<p>
+The best friends list preys upon a similar desire for social validation, but via a public feed rather than shared between individuals. Until 2015, all friends on a user's Snapchat could see their top three contacts. Similar to the streaks feature, this add-on was not central to the utility of the app. Instead, this was a calculated move by Snapchat engineers to profit off of our innate fear of missing out. Psychologist Mitchell Vaterlaus more specifically attributes the effectiveness of the best friends list to the jealousy it incites (Vaterlaus, 2016). Specifically, in the usage of romantic relationships, the best friends list could become very contentious and revealing in certain cases.
+</p>
 
 
 
@@ -45,15 +95,104 @@ include_once('../../inc/paper-heading.php');
 	<div class="container px-0">
 		<div class="row">
 			<div class="col-12">
+
 				<iframe id="my_iframe" width="100%" height="860px" frameBorder="0" src="game/FeedYourFlowers.html" scrolling="no"> </iframe>
 
-				<figcaption class="figure-caption">xxxxx</figcaption>
+				<figcaption class="figure-caption">
+					
+				<details>
+<summary>Feed Your Flowers by Caroline Sigl</summary>
+				
 
+
+
+				<p>
+				Feed Your Flowers is a text-based game about the persuasive nature of the social feed on utilitarian apps. The player is placed into a first-person scenario with little context except for the knowledge that they have recently bought a new house and would like to develop a small garden for the summer. Enter the app, iGardener, downloaded to the player’s phone ever since seeing a targeted ad on Instagram. This app seems like a great resource to directly buy products and build community with other local gardeners. However, as the player spends more time on the feed, their choices grow less their own and more ‘inspired’ by the profuse sending behavior emulated by friends on the app. 
+				</p>
+				
+				<p>
+				The player is given many choices of how to interact with the feed and as well as to close out of the app at any time. However, mimicking the addicting nature of the feed IRL, the player likely will not choose this option and instead let themselves fall even further into the influence of the feed. On the second day of the player’s experience using the iGardener, the game concludes. The player witnesses multiple friends on their feed purchasing a new gardener product that they cannot possibly pass up. In the heat of the moment, the player buys the product. This final interaction with the feed sparks an argument with the player’s fiance since they both agreed to stick to a small budget for hobbies after the move. The game then shows the player reflecting on their experience and how the feed made them feel like the grass was always greener on the other side. 
+				</p>
+
+
+</details></figcaption>
 
 			</div>
 		</div>
 	</div>
 </div>
+
+
+
+<div class="container">
+    <div class="row my-2">
+        <div class="col-12 col-lg-8 offset-lg-2">
+
+
+
+
+
+<h5 class="subheading">Issues</h5>
+
+<p>
+The strong persuasion tactics used by these social-utilitarian apps through feed features lead to unforeseen consequences for the user. In today’s attention economy, companies place a premium placed on increasing time spent on the app as well as converting to in-app purchases. In this section, I will discuss issues that arise from this phenomenon in terms of unintended oversharing, increasingly targeted ads, and breaches of security. 
+</p>
+
+
+<h6>Addiction to Sharing</h6>
+
+<p>
+Interactions on Venmo most poigniantly show the addiction to sharing information on hybrid apps. As put by one Venmo user, Venmo is a type of “weird voyeurism… we’re all interested in seeing what our friends are paying for but at the same time we want to pretend like we don’t care about it” (Levenson, 2014). Users are barraged with the spending and social behavior of others on the social feed so much that they start to emulate this pattern. We might understand this phenomenon as social proof, in which a follower copies the behavior of a high-value person because it has been deemed more appropriate. Social proof in the case of Venmo makes users more likely to share captions of similar specificity to what they may see, for example, from their high school crush who details who they ate lunch with, who they shared a cab with, and what concerts they saw (Levenson, 2014).
+</p>
+
+<p>
+In addition to sharing more, the feed encourages heightened purchasing habits. For example, a friend testified that seeing acquaintances paying each other with inside joke captions made her more likely to pay/request her own friends for small things like a parking meter or a snack from the vending machine. Further, if she saw a Venmo transaction with a caption indicating that an acquaintance got dinner at a new restaurant in town she would be more likely to go with her own friends. This testimony illustrates the power of performative payments to rack up money. While Venmo might appear harmless with cute messages attached to payments, it can drastically realign someone’s spending habits under the guise of ‘keeping up with the Joneses’, or the fear of missing out. 
+</p>
+
+<p>
+The constant pressure to share and buy also takes a toll on the mental health of users of these social-utilitarian apps. Hristova explains that Snapchat streaks can lead to undue stress on relationships and even escalate to arguments if one partner fails to meet the daily communication quota (Hristova, 2022). The extra social features these utilitarian apps add to their interfaces heighten the clash between privacy and saving money vs. social approval through spending. And, in many cases, the social pressure to spend does not come from within but is instead manufactured within the app. 
+</p>
+
+
+<h6>Data Mining</h6>
+
+<p>
+A side-effect of the increased chatter on apps like Snapchat, Venmo, and Spotify is the creation of a massive repository of data for companies to mine. We are convinced to share our location, the people we interact with, and products and lifestyles that we live with the app and outside advertisement firms. By simply checking in at locations (on Snapchat or otherwise), we allow recommender systems to predict our race, gender, age, interests, and economic status (Gábor, 2019). 
+</p>
+
+<p>
+Further, through the social feed on many of these utilitarian apps, we open ourselves up to further detailed data collection. For example, a group of network analysts tracked Venmo posts about gambling and predicted which member had the most money in the group over time (Khanna, 2015). Without the captions enabled, this feat would have been impossible. Instead of directly sending back and forth money, app designers have set the social framework to include additional context for the transaction. And, following peer pressure and social proof, users repeat this behavior. Social networks ultimately mimic the tactics of gambling companies to accumulate finely tuned dossiers on their users to use for ad research or finding the most effective way to appeal to customers. 
+</p>
+
+
+<h6>Security</h6>
+
+<p>
+Public transactions on the social feed can pose severe risks to personal and national security. For example, in 2021, a BuzzFeed investigator located President Biden’s personal Venmo account in ‘less than 10 minutes’ by mapping out connections between White House staffers (Mac 2021). The transactions they unearthed turned out to be harmless transactions between him and his granddaughter. However, the ease with which the public could discover intimate details of the head of state draws attention to the danger of unnecessarily broadcasting information on social/utilitarian apps. Especially with the rise of artificial intelligence, the ability to surveil people on the feed has become even more robust. We create a digital footprint with each of our interactions online and even using the ‘private setting’ can leak information because of public profile information, in the case of Venmo’s friend's list. 
+</p>
+
+
+
+
+
+
+<h5 class="subheading">Solutions</h5>
+
+<p>
+The desire for digital spaces to like, share, and comment has made inevitable the transformation from utilitarian apps to social hybrids with prominent social feeds. Most app users take advantage of the added capability to connect with others and remain up to date on the feed. Instead of placing the responsibility on the consumer to boycott these features, I propose a series of restrictions on social/utilitarian app developers to safeguard the users from coerced excessive sharing. 
+</p>
+
+<p>
+First, each hybrid app should have an easily accessible option to use features of the app in private mode. While most apps appear to have this option, the default sharing setting is usually public, and the private option may still leak personal information. Secondly, I suggest that financial and health-tracking social/utilitarian apps communicate the app's privacy settings even more clearly upon downloading. The money and medical content found in the feed on these apps can be particularly compromising in terms of security and data privacy. And lastly, I recommend that apps readily share the portfolios of information collected on users to sell to advertisers. Finally, I believe that transparency about how companies leverage chatter in the app can be a valuable tool to generate skepticism among users to share on the app. 
+</p>
+
+
+
+
+		</div>
+	</div>
+</div>
+
 
 
 
@@ -70,7 +209,32 @@ include_once('../../inc/paper-heading.php');
 
 		<ol>
 
-		<li>xxxx</li>
+		<li>Albarran, Alan B., and David H. Goff. “A History of the Social Media Industries.” Essay. In The
+		Social Media Industries, 16–45. New York: Routledge, 2013. </li>
+		<li>Gábor Szabó, Gungor Polatkan, Oscar Boykin, and Antonios Chalkiopoulos. “Learn, Map, and Recommend.” Essay. In Social Media Data Mining and Analytics, 245–91. Indianapolis, IN: John Wiley & Sons, 2019. 
+		</li>
+		<li>Hristova D., Lieberoth A. 2021. “How Socially Sustainable Is Social Media Gamification? A Look into Snapchat, Facebook, Twitter and Instagram.” Spanellis A, J. T. H, editors. In Gamification for Organizations and Social Change. Cham: Palgrave Macmillan. pp. 225-245.</li>
+		<li>Hristova, Dayana, Suzana Jovicic, Barbara Göbl, Sara de Freitas, and Thomas Slunecko. “‘Why Did We Lose Our Snapchat Streak?’. Social Media Gamification and Metacommunication.” Computers in Human Behavior Reports 5, no. 100172 (2022). 
+		</li>
+		<li>Khanna A. “Venmo’ed: Sharing Your Payment Data With the World.” Technology Science.
+2015102901. October 28, 2015. https://techscience.org/a/2015102901/.
+</li>
+		<li>Kosmowski, Patrick. “Why Hasn't Spotify Improved Its Social Features?” Medium. UX Collective, June 10, 2020. 
+		</li>
+		<li>Levenson, Eric. “Why the Venmo Newsfeed Is the Best Social Network Nobody's Talking About.” The Atlantic. Atlantic Media Company, April 29, 2014. https://www.theatlantic.com/culture/archive/2014/04/why-the-venmo-newsfeed-is-the-best-social-network-nobodys-talking-about/361342/. 
+		</li>
+		<li>“Spotify: The New Social Network.” Digital Marketing Agency. Campaign Creators, August 26, 
+2022. https://www.campaigncreators.com/blog/spotify-the-social-network/. 
+</li>
+		<li>Mac, Ryan, Katie Notopoulos, Ryan Brooks, and Logan McDonald. “We Found Joe Biden on Venmo. Here's Why That's a Privacy Nightmare for Everyone.” BuzzFeed News. BuzzFeed News, June 19, 2021. https://www.buzzfeednews.com/article/ryanmac/we-found-joe-bidens-secret-venmo. 
+			</li>
+		<li>Vaterlaus, J. Mitchell, Kathryn Barnett, Cesia Roche and Jimmy A. Young. “‘Snapchat is More 
+Personal’: An exploratory study on Snapchat behaviors and young adult interpersonal
+relationships.” Computers in Human Behavior 62 (2016): 594-601.
+</li>
+		<li>Vonderau, Patrick. “The Spotify Effect: Digital Distribution and Financial Growth.” Television & 
+New Media 20, no. 1 (January 2019): 3–19. https://doi.org/10.1177/1527476417741200.
+</li>
 
 		</ol>
 
