8bc15071d2ec08b554f66c548d793313af572ee5
Owen Mundy <omundy@gmail.com>
2024-09-03 13:00:54 -0400

    Adds Howell paper

--- papers/howell/index.php
+++ papers/howell/index.php
@@ -4,32 +4,38 @@ $author = "howell";
 include_once('../../inc/paper-heading.php');
 // print_r($paper);
 
-// NOTES
-// ...
-
 ?>
 
 
 
-
 <div class="container">
+	<div class="row my-2">
+		<div class="col-12 col-lg-8 offset-lg-2">
 
-    <div class="row my-2">
-        <div class="col-12 col-lg-8 offset-lg-2">
-
-
-
-<h5 class="subheading">Introduction</h5>
-
-
+			<figure class="figure w-100">
+				<img src="img/<?php print $paper['thumbnail']; ?>" alt="thumb" class="figure-img w-100 img-fluid">
+				<figcaption class="figure-caption"></figcaption>
+			</figure>
 
 
+<!-- <h5 class="subheading">Introduction</h5> -->
 
 
-<!-- https://drive.google.com/drive/folders/1TKnuCjpXCj_i8AqbesQYrMZNxJgc3Jnw -->
+<p>
+Whether we like it or not, food holds a grip on our lives unlike anything else. Sustenance is required for us to survive, and no matter how strict the diet, everyone must make decisions on what it is that they will eat that day. Somehow, humans must choose from the seemingly infinite number of options that flood the food industry on a daily basis. Moreover, it’s not just about what tastes good, food now comes with baggage—or hidden attachments—making our decision even more complicated. In fact, it’s very rare today to talk about food on a global scale without a crisis being affixed to it. Between global health, ethical production methods, youth wellbeing, sustainable farming, and more, it’s easy to see that what you eat signifies more than just calories. Mixed within all of this exists corporations whose goals often start and end with profit, creating an environment built around cheap production and positive consumer feedback. However, several contradictions exist among mass-produced food products where what is good for individual health and the environment is rarely what is cheap to produce. Further, these conflicts between health and economics are skewed more when considering the impacts of marketing, government lobbying, and cultural norms. Finally, there seems to be an overwhelming stress on advertising to a younger audience, thus exploiting a demographic both unequipped to combat the  highly impressionable strategies of food brands. The intersection of these areas create a dynamic within the food industry that is both unhealthy and unsustainable for the world, yet changing them remains difficult as foods’ ability to continually draw us in is more powerful than ever. 
+</p>
 
+<p>
+The food industry is unquestionably one of the largest in the world. According to the U.S. Department of Agriculture, the foodservice and food retailing industries supplied about $1.79 trillion worth of food in 2019. (Zeballos, 2007) This doesn’t seem to be changing soon either, from 2016-2020 there were over 20,000 new food related products introduced to stores each of those years suggesting that demand is remaining high for new products and new ways to spend money. (Dong, 2021)  Thinking of the food industry as a business makes a lot of sense, but the ramifications of this are just as prominent. There are very few industries that have the kind of ‘constant demand’ that food does, but where this money comes from is an important question. One of the main things to consider is how affordable different foods are to the average consumer, as the logical conclusion is that cheaper food will be bought more often. The figure to the right  shows the price fluctuations with respect to inflation for various categories of food from 1985 to 2000. The studies cited in (Walinga, 2009)  shows that foods like fruits and vegetables which “aid in managing body weight… and reduce the risk of cancer” have drastically increased in price. In stark contrast to these healthier foods, the nutrient-poor foods like “Sugars & sweets”, “Carbonated soft drinks” or “Fats & oils” all are more accessible and affordable to the public in respect to price. This standard that the food industry set in which highly processed foods are affordable for the public set a precedent of overconsumption on these fattier, more sugary, and high caloric foods. This is emphasized by Americans who spent over $970 Billion on food-away-from-home spending in 2019  (Anikka, 2022) likely because of their cheap prices and high caloric meals. Since fast food companies and the foods they represent are able to keep their prices so much lower than that of healthier options, their ability to convince consumers is far greater than anyone else. This context is important to understand as we dive further into what strategies companies use to get people to buy their products. 
+</p>
 
+<p>
+We have established the large presence of foods with higher sugar and caloric levels within the food industry, but this is also what makes up a large portion of the advertising within American news outlets. Consider the most important television advertising day of the year, the Super Bowl. During 2012-2014, food and beverage ads made up over 26% of all advertisements (of any genre) during that period of time with the majority of these relating to unhealthy foods, fast food, or sugary drinks. (Lydecker, 2017) Why is it that these are the images and videos that we are flooded with so aggressively? Conversely, why is this not reciprocated on the other end of the spectrum with the likes of healthy foods or those that are beneficial towards a healthy lifestyle. Advertising is incredibly effective towards these less healthy options, it’s important to consider whether this would work towards other products. 
+</p>
 
+<p>
+Take the advertising ploy of Got Milk? as an example of the power of advertising. “Beginning in earnest after World War II, the US government, schools, and doctors had all promoted milk as a nutritious drink, a necessary component of a healthy diet. By the 1950s, milk had assumed an important role in what people thought was a normal, nutritious lifestyle. In the United States then, milk had been a family staple.” (Ng et al., 2009) This provides a simple case of promoting a narrative surrounding a product creating a very long lasting reputation towards a product. In fact, they did it as a way to directly combat the likes of soda brands and other drinks that were looking to take over the youthful advertising market. The issue of selling milk is that it is boring and not nearly as attractive as other food products, despite the potential for being a very healthy drink alternative. However, through a deliberate advertising effort milk created a brand for itself by using many of the same frameworks that are used for less healthy options.  
+</p>
 
 
 
@@ -43,17 +49,70 @@ include_once('../../inc/paper-heading.php');
 	<div class="container px-0">
 		<div class="row">
 			<div class="col-12">
-				<iframe id="my_iframe" width="100%" height="860px" frameBorder="0" src="game/JustNextDoor.html" scrolling="no"> </iframe>
+				
+				<iframe id="my_iframe" width="100%" height="860px" frameBorder="0" src="game/FoodForThought.html" scrolling="no"></iframe>
 
-				<figcaption class="figure-caption">xxxxx</figcaption>
+				<figcaption class="figure-caption">
 
 
-			</div>
+<details>
+<summary>Food For Thought by Henry Howell</summary>
+
+<p>
+We all have to eat food, it’s necessary to sustain life. Many things that are necessary to life are fairly straightforward; breathing, drinking water, etc. So why is figuring out what to eat so hard these days?? Food For Thought is a game that takes you through a day in the life of a high school student and just trying to get through the day. While attempting to pick out foods to eat, you are bombarded with advertisements of all kinds. Eventually it gets to the point where it’s almost impossible to do anything without feeling the overwhelming weight of advertisements. Ultimately you get to see the effects that not just having to eat sugary foods, but also having to deal with the advertisements that you see on any given day have on your overall health. 
+</p>
+
+<p>
+The goal was to not only emphasize that there are bad food options out there, but also poor strategies of getting people to eat them. Most of the time we are just broken down until we give in eventually. Furthermore, I wanted to use a high school student to show that children and teenagers are attacked more intensely as that group of “customers” are more impressionable to companies. There are certainly ethical ramifications to these that this game doesn’t touch on explicitly but that I wanted to have underlying tones throughout. 
+</p>
+
+<p>
+Finally, one of the most important aspects of the game is the health bar that features on every page. To be able to see how you’re “feeling” after every given decision you make helps you think more clearly about what you’re putting in your body as well as how you’re treating it. This is one of the benefits of playing games, allowing us to pick up on things we might be taking for granted in our own lives. I want people to think about what if they had a health bar constantly following them around as they went through their day and how it would affect their decisions. More specifically, what would be more convincing to yourself; your own health or the advertisers. Food presented an excellent crossroads to look at several different aspects of our lives that force us to consider and think more deeply about. So play Food For Thought!
+</p>
+
+</details></figcaption>
+				
 		</div>
 	</div>
 </div>
 
 
+<div class="container">
+	<div class="row my-2">
+		<div class="col-12 col-lg-8 offset-lg-2">
+
+
+<p>
+The food industry relies on two main persuasion frameworks in order to be successful; social proof and attention. Social proof works off of everyone’s need to fit in with what the people around them are doing while attention assumes people will give more importance to the things they casually focus on more often. (Bragdon, 2017) These two frameworks can summarize the goals of nearly every food advertisement out there. Take McDonald’s for example, the gaudy golden arches not only draw your immediate attention in, but also boast the slogan “Billions and Billions Served” suggesting that your choice to eat there is being backed up by billions of people before you, so it must be the right thing to do. One of the most prominent ways in which advertisers maintain a firm grip on their audience is through their targeting of a younger audience.
+</p>
+
+<p>
+Children are vulnerable when it comes to the many different techniques that food companies use in order to draw them in. Famously in 2011, McDonald’s CEO Jim Skinner claimed that “Ronald McDonald does not advertise unhealthy food to children. We provide many choices that fit with the balanced, active lifestyle. It is up to them to choose and their parents to choose, and it is their responsibility to do so.” (Mallarino et al., 2013) Yes, McDonald’s does offer options that when balanced could fit into a lifestyle that is more healthy than not, but will kids know the intricacies that go into this tightrope walk of healthy living? According to a study run in 2012, around 60-90% of food marketing directed towards children was for pre-sugared breakfast cereals, soft drinks, savory snacks, confectionery and fast foods. (Cairns et al., 2013) Additionally, a study where children wore cameras as a way to see what forms of advertisements they were exposed to the most showed that children interacted with what they called non-core foods 27.3 times a day and core foods only 12.3. (Signal et al., 2017) Once again, the existence of these products do not immediately suggest the presence of unhealthiness in the United States or worldwide, but their constant marketing to the public puts everyone at a distinct disadvantage healthwise. Furthermore, if this is the culture that children are raised in, what are we as a society supposed to expect from them when they grow older? 
+</p>
+
+<p>
+There are efforts to limit marketing directed at children, but it seems like a much more effective strategy would be to shift the content that children are being exposed to. One way this is being accomplished is through the Children’s Food and Beverage Advertising Initiative. (BBBPrograms, 2022) Their main goal is based around their four pillars of: broad industry representation, extensive media coverage, strict uniform nutrition criteria, and strong compliance oversight. Their goal is simply to convince advertisers to limit the presence of foods with  added sugars or sodium and instead include “more food groups or important nutrients like calcium and Vitamin D in foods advertised to children.” (BBBPrograms, 2022) This is a clear example of a large national program taking action to not only combat the state of the food industry, but to also use persuasion architecture to change the system for the better. 
+</p>
+
+<p>
+The ways in which these frameworks are used within healthy food advertising are unique. One study from mdpi (Roose and Mulier, 2020) looked at the way in which healthy foods are advertised as a way of citing the differences between healthy and unhealthy foods, why one is more successful and what’s at the root of that. They specifically looked at sensory advertising, meaning what senses that advertisements attempt to target when advertising. They found that when healthy food ads target only a singular sense (verbal or visual in this case), they are far more successful than if they attempt to target both. It seems as though people can suss out when they are trying to be overly convinced of something good. Tipping the scales towards overtly overdramatizing a product’s positive characteristics can at times be counterproductive as people see through the veil to the perhaps less enticing aspects of healthy foods. Essentially, they found that “the healthy single-sense ads evoke fewer negative thoughts than the healthy multiple-sense visual ad” as can be seen in the figure above. (Roose and Mulier, 2020) It’s the masking of the product in some way that defines healthy advertising. Another study shows this in another way, “healthier foods are not as widely or consistently available as less healthful foods. The fact that convenience stores are so easily accessible makes them an important part of the adolescent food environment. Knowledge of which foods are available is important for research, education, and health promotion.” (Gebauer, 2011) Not only does there need to be creative advertising but there also needs to be a way for people to access the food in question. Putting  healthier foods away from the door subconsciously creates the connection in customers’ heads that they are worse products. The food right next to the cashier doesn't carry added benefits, they simply are more convenient. Once again it seems as though smarter persuasion strategies, such as social proof and attention, could be used towards making healthier foods more appealing to the consumer. 
+</p>
+
+<p>
+It’s important to note that the difference between healthy and unhealthy foods is not exactly binary. In fact it will likely remain a heavily contested question within the food industry and rather than contest that, this paper sought to identify some underlying pieces of food advertising and what can be learned from them. Advertisements represent the persuasion architectures used towards convincing consumers to buy certain products and, as this paper has established, most often those products are associated with unhealthy lifestyles. There is certainly room within this field for the likes of healthier food companies to grow their advertising brand, as can be seen in the example of Got Milk. Furthermore, the impressionable nature of children to food advertisements is quite large and fast food brands and unhealthy food companies alike have largely been exploiting this. However, this is starting to change as nonprofits and government agencies look to limit television advertising towards children and shift the narrative towards more nutritious goods. Overall, asking the food industry to holistically shift to healthier practices is impractical, but there is a lot to be learned from the existing strategies and how they can be applied to healthier products.
+</p>
+
+
+
+
+
+
+
+
+
+		</div>
+	</div>
+</div>
 
 
 
@@ -63,17 +122,30 @@ include_once('../../inc/paper-heading.php');
 	<div class="row my-2">
 		<div class="col-12 col-lg-8 offset-lg-2">
 
-		<h4>Bibliography</h4>
+			<h4>Bibliography</h4>
 
 
-		<ol>
+			<ol>
 
-		<li>xxxx</li>
+			<li>BBBPrograms. “Children's Food & Beverage Advertising Initiative.” BBBPrograms, 1 Jan. 2022, https://bbbprograms.org/programs/all-programs/cfbai#.</li>
+			<li>Bragdon, Trevor. “The Persuasion Wheel.” Medium, Persuasion at Work, 26 June 2017, https://persuasionatwork.com/the-persuasion-wheel-125839dafe35. </li>
+			<li>Cairns, Georgina, et al. "Systematic reviews of the evidence on the nature, extent and effects of food marketing to children. A retrospective summary." Appetite 62 (2013): 209-215.</li>
+			<li>Dong, Xiao. “New Products.” USDA ERS - New Products, 22 Dec. 2021, https://www.ers.usda.gov/topics/food-markets-prices/processing-marketing/new-products.aspx. </li>
+			<li>Gebauer, H., Laska, M.N. Convenience Stores Surrounding Urban Schools: An Assessment of Healthy Food Availability, Advertising, and Product Placement. J Urban Health 88, 616–622 (2011). https://doi.org/10.1007/s11524-011-9576-3</li>
+			<li>Lydecker, Janet A et al. “"I only watch for the commercials": Messages about weight, eating and race in Super Bowl advertisements.” International journal of clinical practice vol. 71,11 (2017): 10.1111/ijcp.13026. doi:10.1111/ijcp.13026</li>
+			<li>Mallarino, Christina, et al. “Advertising of Ultra-Processed Foods and Beverages: Children as a Vulnerable Population.” Revista De Saúde Pública, Faculdade De Saúde Pública Da Universidade De São Paulo, 1 Oct. 2013, https://www.scielo.br/j/rsp/a/rQpr5MvJbFnY3ksFx9pgGMp/?lang=en. </li>
+			<li>Martin, Anikka. “Food Prices and Spending.” USDA ERS - Food Prices and Spending, 22 Mar. 2022, https://www.ers.usda.gov/data-products/ag-and-food-statistics-charting-the-essentials /food-prices-and-spending/. </li>
+			<li>Ng, Daphne, et al. “Got Milk Advertising Strategy.” Are.berkeley.edu, 2009, https://are.berkeley.edu/~sberto/2009Got_Milk.pdf. </li>
+			<li>Roose, Gudrun, and Lana Mulier. "Healthy advertising coming to its senses: The effectiveness of sensory appeals in healthy food advertising." Foods 9.1 (2020): 51.</li>
+			<li>Signal, Louise N., et al. "Children’s everyday exposure to food marketing: an objective analysis using wearable cameras." International Journal of Behavioral Nutrition and Physical Activity 14.1 (2017): 1-11.</li>
+			<li>Wallinga, David. “Today's Food System: How Healthy Is It?.” Journal of hunger & environmental nutrition vol. 4,3-4 (2009): 251-281. doi:10.1080/19320240903336977</li>
+			<li>Zeballos, Eliana, and Wilson Sinclair. “Market Segments.” USDA ERS - Market Segments, 7 Feb. 2007, https://www.ers.usda.gov/topics/	food-markets-prices/food-service-industry/market-segments/.
+			</li>
 
-		</ol>
+			</ol>
 
 
-	    </div>
+		</div>
 	</div>
 </div>
 
