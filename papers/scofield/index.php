95bf56112c56aa952563da7814a285d05a756737
Owen Mundy <omundy@gmail.com>
2024-09-16 13:59:33 -0400

    Codes scofield statement

--- papers/scofield/index.php
+++ papers/scofield/index.php
@@ -4,20 +4,6 @@ $author = "scofield";
 include_once('../../inc/paper-heading.php');
 // print_r($paper);
 
-// NOTES
-
-// Protest Sim is a game meant to illustrate different types of anti-organization architecture. All of these are based around walls and guard positioning (called pigs in game) and protocols. The goal is to get a set number of protesters into a specific area for a set amount of time. In the game, I try to showcase the following types of architecture and protocol:
-
-// -          Placing a guard in front of an access point
-// -          Limiting access points
-// -          Guard patrols
-// -          Flow control
-// -          Redundant fortifications
-
-// Placing a guard in front of an access point is an ancient but effective strategy. If people can’t find a clever way around, they’ll be seen and apprehended. Limiting access points works well with guards. Decreasing the number of access points means fewer guards are needed to cover the area. Patrolling guards further decreases the need to have many guards by allowing one guard to cover a larger area. Flow control is a little more complicated; paths are made thinner and corners are made sharper to slow the traffic through the hallway, road, or corridor and allow enforcement to more easily stop intruders. Redundant fortifications are another classic example of anti-invasion/protest architecture. A popular example of this would be Minas Tirith from The Lord of the Rings. This city is built as a series of semicircles with gates on alternating ends. This means that intruders have to pass through the entire tier in order to reach the gate for the next, since having multiple walls will stop more intruders than just one. This is also a good example of flow control and limiting access points.
-
-// By illustrating abstract examples of this architecture, I hope to enlighten players of its purpose and allow them to identify it in their surroundings. Architecture is a powerful tool for control, but understanding and identifying something is the first step to overcoming it or improving it.
-
 ?>
 
 
@@ -91,8 +77,36 @@ Designs including power dynamics, like the panopticon, exist almost everywhere. 
 			<div class="col-12">
 
 				<iframe id="iframe1" width="100%" height="860px" frameBorder="0" src="game/Build/index.html" scrolling="no"> </iframe>
+				<figcaption class="figure-caption">
 
-				<figcaption class="figure-caption">Protest Sim is a game meant to illustrate different types of anti-organization architecture.</figcaption>
+
+<details>
+<summary>Food For Thought by Henry Howell</summary>
+
+<p>
+Protest Sim is a game meant to illustrate different types of anti-organization architecture. All of these are based around walls and guard positioning (called pigs in game) and protocols. The goal is to get a set number of protesters into a specific area for a set amount of time. In the game, I try to showcase the following types of architecture and protocol:
+</p>
+
+<ul>
+<li>Placing a guard in front of an access point</li>
+<li>Limiting access points</li>
+<li>Guard patrols</li>
+<li>Flow control</li>
+<li>Redundant fortifications</li>
+</ul>        
+
+<p>
+Placing a guard in front of an access point is an ancient but effective strategy. If people can’t find a clever way around, they’ll be seen and apprehended. Limiting access points works well with guards. Decreasing the number of access points means fewer guards are needed to cover the area. Patrolling guards further decreases the need to have many guards by allowing one guard to cover a larger area. Flow control is a little more complicated; paths are made thinner and corners are made sharper to slow the traffic through the hallway, road, or corridor and allow enforcement to more easily stop intruders. Redundant fortifications are another classic example of anti-invasion/protest architecture. A popular example of this would be Minas Tirith from The Lord of the Rings. This city is built as a series of semicircles with gates on alternating ends. This means that intruders have to pass through the entire tier in order to reach the gate for the next, since having multiple walls will stop more intruders than just one. This is also a good example of flow control and limiting access points.
+</p>
+
+<p>
+By illustrating abstract examples of this architecture, I hope to enlighten players of its purpose and allow them to identify it in their surroundings. Architecture is a powerful tool for control, but understanding and identifying something is the first step to overcoming it or improving it.
+</p>
+
+</details></figcaption>
+
+
+
 
 			</div>
 		</div>
