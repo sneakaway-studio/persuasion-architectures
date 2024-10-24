bc325fa608879b39d82c2c988e82dd9d56d08dff
Yumna Ahmed <yuahmed1@davidson.edu>
2024-10-24 15:41:15 -0400

    add img for angelisrecode
    adding images to repo for social proof (de-angelis)

--- papers/de-angelis/angelisrecode/src/data.js
+++ papers/de-angelis/angelisrecode/src/data.js
@@ -1,43 +1,99 @@
 // list items data (28 total)
 export const items = {
   category1: [
-    { text: "Bought an item in the stores many people were recommending", id: "1" },
-    { text: "Followed a trend just because a lot of people were doing it", id: "2" },
+    {
+      text: "Bought an item in the stores many people were recommending",
+      id: "1",
+    },
+    {
+      text: "Followed a trend just because a lot of people were doing it",
+      id: "2",
+    },
     { text: "Filtered out items with ratings of three stars or less", id: "3" },
-    { text: "Adhered to an opinion because it was shared by a lot of people", id: "4" },
-    { text: "Thought something was worth buying because it was almost sold out", id: "5" },
-    { text: "Watched a movie/tv show everyone had been talking about", id: "6" },
-    { text: "Thought a waiter was deserving but did not put money in an empty tip jar", id: "7" }
+    {
+      text: "Adhered to an opinion because it was shared by a lot of people",
+      id: "4",
+    },
+    {
+      text: "Thought something was worth buying because it was almost sold out",
+      id: "5",
+    },
+    {
+      text: "Watched a movie/tv show everyone had been talking about",
+      id: "6",
+    },
+    {
+      text: "Thought a waiter was deserving but did not put money in an empty tip jar",
+      id: "7",
+    },
   ],
   category2: [
-    { text: "Chose to buy something online based on the number and quality of its reviews", id: "8" },
-    { text: "Thanked a bus driver after hearing most of the other passengers doing the same", id: "9" },
-    { text: "Stayed quiet about a concerning situation in a public place because no one else was saying anything", id: "10" },
+    {
+      text: "Chose to buy something online based on the number and quality of its reviews",
+      id: "8",
+    },
+    {
+      text: "Thanked a bus driver after hearing most of the other passengers doing the same",
+      id: "9",
+    },
+    {
+      text: "Stayed quiet about a concerning situation in a public place because no one else was saying anything",
+      id: "10",
+    },
     { text: "Ate something at a buffet because it was almost gone", id: "11" },
-    { text: "Signed up for a service based on the number of people already using it", id: "12" },
-    { text: "Considered a restaurant/store good because it always had a line outside the door", id: "13" },
-    { text: "Liked a post just because it already had a lot of likes", id: "14" }
+    {
+      text: "Signed up for a service based on the number of people already using it",
+      id: "12",
+    },
+    {
+      text: "Considered a restaurant/store good because it always had a line outside the door",
+      id: "13",
+    },
+    {
+      text: "Liked a post just because it already had a lot of likes",
+      id: "14",
+    },
   ],
   category3: [
-    { text: "Bought an item because it was a 'best-seller' or 'fastest growing'", id: "15" },
+    {
+      text: "Bought an item because it was a 'best-seller' or 'fastest growing'",
+      id: "15",
+    },
     { text: "Recreated a recipe have seen a lot of people try", id: "16" },
-    { text: "Joined a line even though did not know what it was for", id: "17" },
+    {
+      text: "Joined a line even though did not know what it was for",
+      id: "17",
+    },
     { text: "Found a show funnier thanks to canned laughter", id: "18" },
     { text: "Visited a not-so-exciting but trendy place", id: "19" },
     { text: "Watched a video just because it was trending", id: "20" },
-    { text: "Bought something because it had many likes", id: "21" }
+    { text: "Bought something because it had many likes", id: "21" },
   ],
   category4: [
-    { text: "Got convinced by the sign to reuse the towel in a hotel", id: "22" },
-    { text: "Donated to a charity after getting told a lot of people were donating", id: "23" },
-    { text: "Did not comment an interesting post because no one else had commented it", id: "24" },
+    {
+      text: "Got convinced by the sign to reuse the towel in a hotel",
+      id: "22",
+    },
+    {
+      text: "Donated to a charity after getting told a lot of people were donating",
+      id: "23",
+    },
+    {
+      text: "Did not comment an interesting post because no one else had commented it",
+      id: "24",
+    },
     { text: "Bought an item from the 'recommended to me' section", id: "25" },
-    { text: "Clapped at a live show following the rest of the audience's example", id: "26" },
-    { text: "Watched a movie/tv show that appeared in the 'most popular' list", id: "27" },
-    { text: "Ate at a restaurant many people were recommending", id: "28" }
-  ]
+    {
+      text: "Clapped at a live show following the rest of the audience's example",
+      id: "26",
+    },
+    {
+      text: "Watched a movie/tv show that appeared in the 'most popular' list",
+      id: "27",
+    },
+    { text: "Ate at a restaurant many people were recommending", id: "28" },
+  ],
 };
-
 
 // result text and image data (3 total)
 export const results = [
@@ -50,8 +106,7 @@ export const results = [
     description:
       'You do not really pay attention to the number of comments, likes and reviews on online platforms. Or maybe you do, but they are not the factors that control your actions online. You would rather watch something without hearing how other people react to it, and you would rather not follow the crowd just because "everyone else is doing it."',
     media: {
-      image:
-        "https://img.buzzfeed.com/buzzfeed-static/static/2020-04/23/17/enhanced/6bcbd806692c/enhanced-392-1587661661-2.jpg",
+      image: "images/arrow-by-people.jpg",
     },
     credit: "axerosolutions.com / Via axerosolutions.com",
     credit_url:
@@ -66,8 +121,7 @@ export const results = [
     description:
       "Of course you use social media and look at how people react to the content, buy products online and read their reviews. But, you are able to not get influenced too much by the way others use online platform, and can often decide what to do based on what you believe is best. Aside from the online space, you visit popular places, do activities recommended by many, and follow trends, but not just merely because the majority of people suggest doing so, also if you personally think they are worthy.",
     media: {
-      image:
-        "https://img.buzzfeed.com/buzzfeed-static/static/2020-04/23/17/enhanced/bc71f6de0007/enhanced-360-1587661566-3.jpg",
+      image: "images/group-huddle-social-feed.jpg",
     },
     credit: "dreamhost.com / Via dreamhost.com",
     credit_url:
@@ -81,8 +135,7 @@ export const results = [
     description:
       "Posts, images, videos and products seem less interesting and less valid to you if the number of likes, or similar positive reinforcement, is low. You consider the numbers on different online platform to be the main indication of the quality of the content posted there. You are more likely to engage in activities if you see that many other people, especially if they are your friends or similar to you, are also doing them. Additionally, you would feel lost watching a sitcom without the laugh track, and appreciate shows with a live audience, so that the jokes sound funnier and you get a sense of when it is the right time to laugh. In real life, you think lines outside of stores are a clear symbol of good quality and you always try the new trend if many people around you are recommending it.",
     media: {
-      image:
-        "https://img.buzzfeed.com/buzzfeed-static/static/2020-04/30/20/enhanced/98bcd8c28f1d/enhanced-831-1588279128-25.jpg",
+      image: "images/icons-surround-person.jpg",
     },
     credit:
       "sehealthcarequalityconsulting.com / Via sehealthcarequalityconsulting.com",
