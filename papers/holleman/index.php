81e6eca74390f3dfd0a21b2f5b39d0746bb79d24
Owen Mundy <omundy@gmail.com>
2024-11-14 13:21:18 -0500

    Adds Holleman essay and a game version (correct?)

--- papers/holleman/index.php
+++ papers/holleman/index.php
@@ -4,9 +4,6 @@ $author = "holleman";
 include_once('../../inc/paper-heading.php');
 // print_r($paper);
 
-// NOTES
-// ...
-
 ?>
 
 
@@ -15,35 +12,59 @@ include_once('../../inc/paper-heading.php');
 
 <div class="container">
 
-    <div class="row my-2">
-        <div class="col-12 col-lg-8 offset-lg-2">
+	<div class="row my-2">
+		<div class="col-12 col-lg-8 offset-lg-2">
+
+			<?php include_once('../../inc/paper-thumbnail.php'); ?>
 
 
 
-<h5 class="subheading">Introduction</h5>
+			<h5 class="subheading">Introduction</h5>
 
+			<p>
+				If I asked you to list five children’s movies, what would come to mind? Chances are, at least one of them was made by Disney or Pixar. Millions of people worldwide have a strong, nostalgic connection to the company and its creations, whether that be Mickey Mouse, the princesses, Nemo, or Phineas and Ferb. What if I told you there’s a place where you can meet all of these characters in real life? Of course you know what I’m referring to. Through billions of dollars worth of marketing, the Disney corporation has made Disney World synonymous––to most Americans, at least––with “the happiest place on earth.” Clearly, millions of people agree, or at least want to see for themselves: Disney’s parks and resorts amassed over 43 million visitors last year, making the company $16.97 billion.
+			</p>
 
+			<p>
+				This success begs the question: what is Disney doing right? Why is a trip to the Disney Parks so sought after by American consumers? Park guests seek nostalgia, a connection to their inner-child, and an escape from reality. The environment fulfilles the guests’ desires through persuasion and manipulation. Visitors willingly participate in this manipulation by performing within the space’s social expectations––they wear costumes, meet beloved fictional characters, and pose in front of fake castles. Most guests, however, are not fully aware of the many forms of manipulation they passively intake through their senses. This immersion takes many forms, constantly present as a guest maneuvers throughout the Park. Although guests visit Disney Parks with the expectation of entering a manipulated reality, Disney parks cast a spell over their guests by creating an experience of full fantasy through sensory persuasion.
 
-
-
-<!-- 
-
-Hi everyone, here is my final game and documentation. A lot more of my game materials and my full Game Design Doc are in the Google Drive folder! 
-
-Game statement: 
-
-
-Take a second to imagine Disney World. Even if you’ve never been, you can likely still hear the chirpy songs, feel the heat and the crowds, and imagine the princesses and Mickeys posing with fans. Disney has produced universally recognizable characters, images, and styles, and their parks allow guests to physically interact with these beloved and nostalgic creations. Magical Manipulation explores sensory manipulation in a day at Disney World. I wanted to explore a more lighthearted type of persuasion, one that people knowingly and readily give in to. A visit to a Disney Park is an extremely immersive experience, based in sensory intake. Different manipulation tactics like forced perspective, smelltizers, and precise theming are omnipresent in Disney Parks. I incorporated some of these sensations into my game by using specific colors, sounds, and images to invoke emotional responses, just as the Parks do. Through Magical Manipulation, players can experience the magic of Disney tricks in the comfort of their own homes. 
-
-As your character moves through the park, the game aesthetics change to reflect different thematized areas. The game takes place in the Magic Kingdom, and the sounds and images change based on your location within the park. The song as you near Cinderella’s castle, for example, sounds nothing like the queue music for the Seven Dwarfs train ride, which is because Disney intentionally keeps all of its themes distinctly separate. To make gameplay more interactive and immersive, I added a couple of point-of-view YouTube videos of different rides. Because I wanted to cover a wide variety of sensory tricks in the park, the product is more of an interactive than a “game” because there is no real objective other than experiencing Disney World. There is only one outcome, but the game is replayable because there are still many choices to be made and much to explore within it! I hope you enjoy the Magical Manipulation of Disney.  
-Attached is my video intro, the html link to my game, and the cover photo which I pulled from a public domain image site.  -->
+			</p>
 
 
 
 
 
+			<h5 class="subheading">Analysis</h5>
 
-		</div>
+
+			Customers pay Disney prices for the Disney experience. Even as the corporation consistently raises admission prices, fans continue to pay high premiums because Disney offers such a unique adventure. As George Harwell of the Washington Post writes, “The fun-per-hour value of the experience is still high, Disney executives say, because a tourist can spend all day there (as opposed to, say, a blockbuster movie). They add that higher gate prices have helped Disney invest in new, better attractions.” New attractions help Disney persuade people to visit their parks––or at least continue coming back––continuing the cycle of inflated experiential value. Throughout this paper, I explore the methods Disney employs to keep their Parks “worth it” for visitors. We begin, however, by analyzing how guests earnestly involve themselves in this process of manipulation.
+
+
+
+			<h5 class="subheading">Committing to the Magic</h5>
+
+			<p>
+				Performance theory plays an integral role in understanding why and how Disney guests commit to an active role in the fantasy. Erving Goffman, a Canadian sociologist who rose to prominence in the 1950s, originated ideas of social interaction as a type of performance. He explained that everyone participates in an independent performance everyday, without knowing, as we present a “front” to conform to social expectations especially in public spaces, and a “back” when either no one is around or just people we trust and find comfort with. The expectations and standards of different social settings affect how we present this “front”. This concept of performance takes on a double meaning in the theme park setting: the park itself tells a story and puts on a show, and guests behave and act accordingly.
+			</p>
+
+
+
+
+			<h5 class="subheading">The Disney Experience: What Makes it so Magical?</h5>
+
+			<p>
+				As a tourist destination, Disney promises guests an escape from the mundaneness of their day-to-day lives. Although the park employees are there to earn a living, they must continuously perform for guests. To maximize this departure from reality, guests become co-stars in this performance. Consequently, Walt Disney revolutionized theme park tourism by restructuring the company-customer relationship. This accomplishment was primarily achieved through rebranding staff as “cast-members,” and encouraging customers to become actors in the fantastical world of Disney.
+			</p>
+
+			<p>
+				To this end, some scholars have pegged the Disney Park experience as one of “immersive theater,” and question the extent to which guests have agency within the story. Because Disney employees follow scripts and operate under a corporate system driven by profit, customers often just need to “complete” the story through their interaction, rather than drive or write the story themselves. There is no clear answer to this question of guest agency, which has been debated by many scholars in Disney-related work and academia. Performance experts Jennifer Kokai and Tom Robson disagree with other scholars who view Disney Park guests as “p​​assive recipient of commercialized falseness,” but rather argue that Disney is “essentially constructing enormous immersive theater spaces where guests perform as actors,” and serve as voluntary and enthusiastic participants. The concept of Disney guests as “actors” begins all the way back in 1955, at the revolutionary opening of Disneyland.
+			</p>
+
+
+
+
+
+			</div>
 	</div>
 </div>
 
@@ -52,9 +73,24 @@ Attached is my video intro, the html link to my game, and the cover photo which 
 	<div class="container px-0">
 		<div class="row">
 			<div class="col-12">
-				<iframe id="iframe1" width="100%" height="860px" frameBorder="0" src="game/JustNextDoor.html" scrolling="no"> </iframe>
+				<iframe id="iframe1" width="100%" height="860px" frameBorder="0" src="game/Magical Manipulation.html" scrolling="no"> </iframe>
 
-				<figcaption class="figure-caption">xxxxx</figcaption>
+				<figcaption class="figure-caption">
+					<details>
+						<summary>The Manipulative Magic of Disney by <?php print $paper["author"]; ?></summary>
+
+
+						<p>
+							Take a second to imagine Disney World. Even if you’ve never been, you can likely still hear the chirpy songs, feel the heat and the crowds, and imagine the princesses and Mickeys posing with fans. Disney has produced universally recognizable characters, images, and styles, and their parks allow guests to physically interact with these beloved and nostalgic creations. Magical Manipulation explores sensory manipulation in a day at Disney World. I wanted to explore a more lighthearted type of persuasion, one that people knowingly and readily give in to. A visit to a Disney Park is an extremely immersive experience, based in sensory intake. Different manipulation tactics like forced perspective, smelltizers, and precise theming are omnipresent in Disney Parks. I incorporated some of these sensations into my game by using specific colors, sounds, and images to invoke emotional responses, just as the Parks do. Through Magical Manipulation, players can experience the magic of Disney tricks in the comfort of their own homes.
+						</p>
+
+						<p>
+							As your character moves through the park, the game aesthetics change to reflect different thematized areas. The game takes place in the Magic Kingdom, and the sounds and images change based on your location within the park. The song as you near Cinderella’s castle, for example, sounds nothing like the queue music for the Seven Dwarfs train ride, which is because Disney intentionally keeps all of its themes distinctly separate. To make gameplay more interactive and immersive, I added a couple of point-of-view YouTube videos of different rides. Because I wanted to cover a wide variety of sensory tricks in the park, the product is more of an interactive than a “game” because there is no real objective other than experiencing Disney World. There is only one outcome, but the game is replayable because there are still many choices to be made and much to explore within it! I hope you enjoy the Magical Manipulation of Disney.
+						</p>
+
+
+					</details>
+				</figcaption>
 
 
 			</div>
@@ -72,17 +108,67 @@ Attached is my video intro, the html link to my game, and the cover photo which 
 	<div class="row my-2">
 		<div class="col-12 col-lg-8 offset-lg-2">
 
-		<h4>Bibliography</h4>
 
 
-		<ol>
 
-		<li>xxxx</li>
+			<p>
+				When Disneyland first opened, employees at the Anaheim park welcomed visitors as guests and contributed to the fantasy by costuming as beloved characters. The park’s consistent theming was extremely innovative, especially in comparison to the amusement and trolley parks of the time. As author and expert on American culture Sabrina Mittermeier writes in her book A Cultural History of the Disneyland Theme Parks: Middle-Class Kingdoms, “Everyone who worked at the park wore a costume, not a uniform. To stand out from amusement parks, every visitor was a guest, not a customer. Guests had been invited to visit Disneyland and they were all VIPs. There were no rides, rather attractions, adventures, or experiences”. This system plays into the make-believe of the Park experience, overshadowing the inherently corporate relationship between the company, its employees, and its customers. This sense of leaving the real world and entering a new reality is exacerbated not just by staff and visitors, but also from the physical park itself.
+			</p>
 
-		</ol>
+			<p>
+				The Park environment begins to manipulate guests’ senses the second they walk in. Once past security, guests immediately Walt Disney and his team came up with the hub-and-spoke layout for Disneyland, which is used by all of the company’s parks to date. This layout allows guests to enter the main park plaza through one entrance only, and from that central point, other attractions arranged radially for easy access and short walking distance from the plaza. As a guest walks through different areas of the park, the topography of the pavement changes to match the theme of each section, which is intended to “manipulate the guest into a particular engagement with the environment.” Disney even hides easter eggs on the sidewalks related to the corner you’re in: peanuts on the sidewalk near Dumbo, hoof prints near Fantasyland, and so on. The physical layout and intentionally designed environment of Disney Parks keeps the fantasy consistent throughout the entire park.
+			</p>
 
 
-	    </div>
+			<h5 class="subheading">Manipulated Senses</h5>
+
+			<p>
+				Guests experience the magic of Disney through many senses. Music, forced perspectives, and smells around Disney parks are all carefully crafted by the company. Music plays an especially important role in creating a sense of nostalgia from guests and evoking different emotional responses from visitors as they move about the park. In his article, “‘Whole New Worlds’: Music and the Disney Theme Park Experience,” Charles Carson, a professor of music at the University of Texas at Austin explains the three categories of music played during a day at the park.
+			</p>
+
+			<blockquote>
+				There are essentially three types of musical events on display in the Disney parks: 1) "piped in" background music (in the form of themed "area" music, but also the theme music for individual rides and attractions); 2) live music (including music performed by roving musicians, as well as certain live music performances on stage); and 3) pre-recorded performances which may or may not include a live element (such as parades, character-based shows, filmed attractions or "special effect" shows).
+			</blockquote>
+
+			<p>
+				These carefully crafted “musical events” highlight how intentionally Disney plans sound design in their parks. The music played on speakers throughout the park relates directly to the theme of a given area. Both pre-recorded and live music often play into themed events or environments as well, such as the Irish and Blues bands that often play at Disney Springs’ Raglan Roads Irish Pub and House of Blues, respectively. Carson explains that Disney chooses these genres to “appeal to a broader public,” by avoiding music that could be interpreted as highbrow or alienating. The effect of these genres, he argues, is that guests agree to believe in the Disney version of the various cultures and experiences they portray in a given park’s different themed sections. Accessible, public-friendly music is just one of many important factors in how Disney achieves an agreement of manipulated reality with its park guests.
+			</p>
+
+			<p>
+				Visual tricks and optical illusions appear everywhere in Disney Parks. Perhaps the most famous example of this is the forced perspective of Cinderella’s Castle in Disney World. Using specific scales, ratios, and angles, this landmark was built to appear much taller and more distant than it actually is. Disney employs forced perspective on many notable monuments, such as Splash Mountain, Beast’s Castle, Hollywood Towers, and the World of Avatar. Further, each themed section of the park is visually cut off from the others, so that guests remain fully immersed in the story of whatever area they are in. This is achieved by a variety of visual tricks as well as by positioning queues and attractions so that guests cannot see beyond barriers.
+			</p>
+
+			<p>
+				Similarly, Disney uses “smelltizers” to bring theme-specific aromas to certain areas of the park. These smells are used for two purposes: to immerse guests into the theme of attractions, and to attract guests to restaurants and food stands. Smells in attractions and their queues help make a fully immersive experience for guests, adding another layer of detachment from reality. Whereas a musty, mildewey scent fills Haunted Mansion and Pirates of the Caribbean, a fresh citrus fills Soarin’, fitting the imagined universe for each ride. Smelltizers outside restaurants pump out artificial yet delightful scents for guests to waft through, hoping to get them to stop and consider stepping inside to buy a snack. These smells include popcorn, vanilla, cinnamon, and even change seasonally, with a pine aroma taking prominence around Christmas. Scent is a subtle yet powerful sense through which Disney convinces its guests to buy into the fantasy, and buy their food goods.
+			</p>
+
+
+
+
+			<h5 class="subheading">Conclusion</h5>
+
+			<p>
+				All of these sensory tricks contribute to the magic of Disney, and in turn, persuade guests to “perform” in accordance with the manipulated fantasy environment. While colors, smells, and music are all passively perceived by guests, this intake is still voluntary because guests both hope for and expect these forms of persuasion when they sign up to visit a Disney Park. The manipulated environment of a Disney Park encourages guests to present a “front” that coincides with the fantasy, and perform socially to fit into the setting. This dynamic not only allows but encourages guests to participate in the Park’s story and its imagined environment. Every place we visit sets a certain precedent for how we are supposed to act. The next time you find yourself at work, a party, or even a Disney Park, consider the setting’s subconscious cues that suggest how you should “perform,” and whether you find yourself persuaded by them.
+			</p>
+
+
+			<h4>Bibliography</h4>
+
+
+			<ol>
+
+				<li>Carson, Charles. “‘Whole New Worlds’: Music and the Disney Theme Park Experience.” Ethnomusicology Forum, vol. 13, no. 2, [Taylor & Francis, Ltd., British Forum for Ethnomusicology], 2004, pp. 228–35, http://www.jstor.org/stable/20184483. </li>
+				<li>Gennawey, Sam, and Jeff Kurtti. Disneyland Story: The Unofficial Guide to the Evolution of Walt Disney’s Dream. Unofficial Guides, 2013. https://ebookcentral-proquest-com.proxy048.nclive.org/lib/davidson/detail.action?pq-origsite=primo&docID=1486410 </li>
+				<li> Kokai, Jennifer A., and Tom Robson. Performance and the Disney Theme Park Experience: The Tourist As Actor. Springer International Publishing AG, 2019, https://doi.org/10.1007/978-3-030-29322-2. </li>
+				<li>McCormick, Mary Helene. “Performing Disney: Exploring Guests’ Narratives of Disney Theme Parks”. UiT The Arctic University of Norway, 2021.</li>
+				<li>Mittermeier, Sabrina. A Cultural History of the Disneyland Theme Parks : Middle-Class Kingdoms. Intellect, 2020. </li>
+				<li>Moore, Alexander. “Walt Disney World: Bounded Ritual Space and the Playful Pilgrimage Center.” Anthropological quarterly 53, no. 4 (1980): 207–218.</li>
+				<li>Shepherd, Simon. The Cambridge Introduction to Performance Theory. Cambridge: Cambridge University Press, 2016.</li>
+
+			</ol>
+
+
+		</div>
 	</div>
 </div>
 
